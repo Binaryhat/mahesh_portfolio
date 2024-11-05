654a47ecb1324db4c6ebc200465d8bd4d168528a
binaryhat <binaryhat18@gmail.com>
2024-11-05 22:16:57 +0530

    quote_generator-corrected

--- quote_generator/script.js
+++ quote_generator/script.js
@@ -3,59 +3,80 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
-const loader= document.getElementById('loader');
+const loader = document.getElementById('loader');
 
-function showLoadingSpinner(){
-    loader.hidden=false;
-    quoteContainer.hidden=true;
-}
-function removeLoadingSpinner(){
-    if(!loader.hidden){
-        quoteContainer.hidden=false;
-        loader.hidden=true;
-    }
-}
-//Get Quote from API
-async function getQuote(){
-   showLoadingSpinner();
-    const proxyUrl='https://cors-anywhere.herokuapp.com/';
-    const apiUrl='http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-    try{
-        const response= await fetch(proxyUrl + apiUrl);
-        const data = await response.json();
-        quoteText.innerText= data.quoteText;
-        //if author is blank, add unknown
-        if(data.quoteAuthor===''){
-            authorText.innerText='Unknoown';
-        }else{
-            authorText.innerText= data.quoteAuthor;
-        }
+// -----> If you're using the localQuotes array you can remove apiQuotes variable
 
-        // Reduce font size for long quotes
-        if(data.quoteText.length >120){
-            quoteText.classList.add('long-quote');
-        }else{
-            quoteText.classList.remove('long-quote');
-        }
-        console.log(data);
-        //stop loader, show the quote
-        removeLoadingSpinner();
-    }catch(error){
-        getQuote();
-        console.log('Woops, no quotes ', error);
-    }
+let apiQuotes = [];
+
+// Loading Spinner Shown
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
 }
 
-//post quote on twitter 
-function tweetQuote(){
-    const quote = quoteText.innerText;
-    const author = authorText.innerText;
-    const twitterUrl= `https://twitter.com/intent/tweet?text = ${quote} - ${author}`;
-    window.open(twitterUrl,'_blank');
+// Remove Loading Spinner
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
 }
-//Event Listener
-newQuoteBtn.addEventListener('click',getQuote);
+
+// Show New Quote
+function newQuote() {
+  loading();
+
+  // -----> Only use one of the following const quote statements
+
+  // Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+  // Pick a random quote from localQuotes array
+  // const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+
+  // Check if Author field is blank and replace it with 'Unknown'
+  if (!quote.author) {
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // Check Quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+  // Set Quote, Hide Loader
+  quoteText.textContent = quote.text;
+  complete();
+}
+
+// -----> If you're using the localQuotes array you can remove getQuotes() function 
+
+// Get Quotes From API 
+async function getQuotes() {
+  loading();
+  const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.innerText} - ${authorText.innerText}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
-//on Load
-getQuote();
+// On Load
+getQuotes();
+
+// -----> If using API, run getQuotes(), if not run newQuote() instead
+// newQuote();
